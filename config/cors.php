0e574d3bcd0aefc36b1e00e6cde2f16aad213be7
ErickjGonzalez <erickjgonzalez117@gmail.com>
2026-06-12 21:12:59 -0600

    add

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://censo-front-end.vercel.app/inicio'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
